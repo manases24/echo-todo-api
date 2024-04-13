74d7555bef0cf3ced9c886d7ac9c3badb1dcce27
mnsh5 <hugo.orlando2050@gmail.com>
2024-04-13 16:33:03 -0300

    :sparkles: add new updates

--- main.go
+++ main.go
@@ -1,8 +1,26 @@
 package main
 
-import "github.com/labstack/echo/v4"
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/mnsh5/todo-api/db"
+	"github.com/mnsh5/todo-api/routes"
+)
 
 func main() {
 	e := echo.New()
+	db.ConnectDB()
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Middleware CORS
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowMethods:     []string{echo.GET, echo.POST, echo.PUT, echo.PATCH, echo.DELETE},
+		AllowHeaders:     []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+		AllowCredentials: true,
+	}))
+	routes.SetupRoutes(e)
 	e.Logger.Fatal(e.Start(":2024"))
 }
--- services/task_service.go
+++ services/task_service.go
@@ -7,7 +7,7 @@ import (
 type TaskService interface {
 	Create(c echo.Context) error
 	Update(c echo.Context) error
-	Delete(c echo.Context) error
+	// Delete(c echo.Context) error
 	FindById(c echo.Context) error
 	FindAll(c echo.Context) error
 }
