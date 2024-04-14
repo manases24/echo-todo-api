673f83981ae8204826a015c1fe6a89ced508f16a
mnsh5 <hugo.orlando2050@gmail.com>
2024-04-14 18:31:49 -0300

    :rocket: add new updates

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,12 @@
 import { Header } from "./components/Header";
 import { TaskCreate } from "./components/TaskCreate";
-// import { TaskResult } from "./components/TaskResult";
 
 function App() {
   return (
     <div className="container mx-auto px-4 my-2.5">
       <Header />
       <TaskCreate />
+
     </div>
   );
 }
